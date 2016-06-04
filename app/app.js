231af377c4cedcc7cc154c3d69d3427a76f8b019
James McCray <chrisjamesjackmccray@gmail.com>
2016-06-04 00:57:38 -0500

    Simple Three Page Boilerplate

--- app/app.js
+++ app/app.js
@@ -3,14 +3,20 @@ import uiRouter from 'angular-ui-router';
 
 import firebase from 'firebase';
 import angularFire from 'angularfire';
+import angularScroll from 'angular-scroll';
+import whole from './modules/whole';
 
 let App = angular.module('app', [
   'ui.router',
-  'firebase'
+  'firebase',
+  'duScroll',
+  'tiy.whole',
 ]);
 
 function config($urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
 }
+
+App.value('duScrollOffset', 60);
 
 App.config(config);
